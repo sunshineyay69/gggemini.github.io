713c8dbb856fe6650c539b0bd5d2122cfe7e6408
TanchikiPro <tanchikipro7777777@gmail.com>
2021-03-07 12:26:41 +0300

    Hello, my wife

--- js/main.js
+++
@@ -1,9 +0,0 @@
-function RemoveShit() {
-    let scripts = document.getElementsByTagName("script");
-    scripts[0].parentNode.removeChild(scripts[scripts.length - 1]);
-
-    let divs = document.getElementsByTagName("div");
-    document.body.removeChild(divs[divs.length - 1]);
-}
-
-//setTimeout(RemoveShit, 100);
